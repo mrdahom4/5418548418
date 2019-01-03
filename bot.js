c068a241e926466d8efe99a0abb739c97173a2e4
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2019-01-03 17:23:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,31 +26,58 @@ client.on('ready', () => {
   console.log('')
 });
 
-const myDiscriminator = ["0000", "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0069"];//حطا التاق هناconst bot = new(require("discord.js")).Client({fetchAllMembers: true});
-//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes//Toxic Codes
-const password = "01067054858";//بسورد حسابك عليشن الكود يقدر يغير الاسم
-const token = "NTI1NDM0NTQ4OTM5NjUzMTUx.DwgOvQ._KZFdGVu58UPpECQ8tNpoWMEgwk"; // توكن حسابك
-//Toxic Codes
-//Toxic Codes
-function changeDiscriminator() {
-  if (myDiscriminator.includes(bot.user.discriminator.toString()))
-    return console.log("Discriminator Loaded: " + bot.user.discriminator);
-  try {//Toxic Codes
-    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
-    console.log(Date.now(), "Username Loaded: " + us);
-    bot.user.setUsername(us, password).then((u) => {
-      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
-      setTimeout(changeDiscriminator, 8640 * 10000);
-    }).catch((err) => {//Toxic Codes
-      console.log(Date.now(), "An error occurred. Trying again in sixty (60) seconds.");
-      setTimeout(changeDiscriminator, 60 * 1e3);
-    });//Toxic Codes
-  } catch(e) {//Toxic Codes
-    console.log(Date.now(), `[${e}] Trying again in 30 seconds.`);
-    setTimeout(changeDiscriminator, 30 * 1e3);
-  }//Toxic Codes
-}//Toxic Codes
- 
+
+ const devs = ["520629080333352976"]
+const adminprefix = "Z";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+     
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();   
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/zero");
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  }
+  if (message.content.startsWith(adminprefix + 'na')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✅   ${argresult}** `)
+} else
+  if (message.content.startsWith(adminprefix + 'prefix')) {//Toxic Codes
+  client.user.setPrefix(argresult).then
+      message.channel.send(`**✅   ${argresult}** `)//Toxic Codes
+} else
+if (message.content.startsWith(adminprefix + 'av')) {//Toxic Codes
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✅   ${argresult}** `);//Toxic Codes
+}
+});
+
+client.on("message", message => {
+
+            if (message.content.startsWith("fjgklfjd")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 client.login(process.env.BOT_TOKEN);
